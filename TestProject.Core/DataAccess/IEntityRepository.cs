1d3d66e81a37597804cbc62d8a49aa147c62d5e9
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-07 22:12:36 +0300

    Add project files.

---
+++ TestProject.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Core.Entities;
+
+namespace TestProject.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class, IEntity,new()
+    {
+        T Add(T entity);
+        Task<T> AddAsync(T entity);
+    }
+}
