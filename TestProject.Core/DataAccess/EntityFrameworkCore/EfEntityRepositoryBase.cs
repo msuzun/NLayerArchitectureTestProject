3a9629d25fcf50b744be5c8ac25c90d4e5474877
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-07 22:54:32 +0300

    Generic repository adımları

---
+++ TestProject.Core/DataAccess/EntityFrameworkCore/EfEntityRepositoryBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Core.Entities;
+
+namespace TestProject.Core.DataAccess.EntityFrameworkCore
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity> 
+        where TEntity:class, IEntity,new()
+        where TContext: DbContext,new()
+    {
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestProject.Core/DataAccess/IEntityRepository.cs
+++ TestProject.Core/DataAccess/IEntityRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using TestProject.Core.Entities;
@@ -10,5 +11,11 @@ namespace TestProject.Core.DataAccess
     {
         T Add(T entity);
         Task<T> AddAsync(T entity);
+        T Update(T entity);
+        Task<T> UpdateAsync(T entity);
+        void Delete(T entity);
+        Task DeleteAsync(T entity);
+        T Get(Expression<Func<T, bool>> filter = null);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
     }
 }
