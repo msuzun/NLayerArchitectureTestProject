7d78eac0de63a5a2b00483ccec64c84e8994c94e
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-08 15:14:16 +0300

    Categor ve Product için manager sınıflar yazıldı

---
+++ TestProject.Business/Concreate/Manager/ProductManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Business.Abstract;
+using TestProject.DataAccess.Abstract;
+using TestProject.Entity.Concreate;
+
+namespace TestProject.Business.Concreate.Manager
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+            return await _productDal.AddAsync(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(d => d.Id == id);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetListByCategoryId(int categoryId)
+        {
+            return _productDal.GetAll(d => d.CategoryId == categoryId);
+        }
+
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            return await _productDal.UpdateAsync(product);
+        }
+    }
+}
