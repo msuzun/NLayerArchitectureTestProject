0d9dc593f58251e0906ad1be5f1912345afecbf6
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-08 14:56:23 +0300

    CagegoryDal ve Product Dal interface ve concreate'lar olşuşturuldu

---
+++ TestProject.DataAccess/Concreate/EntityFrameworkCore/EfCategoryDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Core.DataAccess.EntityFrameworkCore;
+using TestProject.DataAccess.Abstract;
+using TestProject.Entity.Concreate;
+
+namespace TestProject.DataAccess.Concreate.EntityFrameworkCore
+{
+    public class EfCategoryDal : EfEntityRepositoryBase<Category, TestProjectDbContext>, ICategoryDal
+    {
+       
+    }
+}
