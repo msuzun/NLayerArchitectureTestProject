5b7d96a8c231666c3f2879a4fd08a680949844ea
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-09 01:50:55 +0300

    business servislere productImage eklendi

---
+++ TestProject.Business/Abstract/IProductImageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Entity.Concreate;
+
+namespace TestProject.Business.Abstract
+{
+    public interface IProductImageService
+    {
+        ProductImage Add(ProductImage productImage);
+        ProductImage Update(ProductImage productImage);
+        void Delete(ProductImage productImage);
+        ProductImage GetById(int id);
+        List<ProductImage> GetList();
+        List<ProductImage> GetListByProductId(int productId);
+
+    }
+}
---
+++ TestProject.Entity/Concreate/ProductImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Core.Entities;
+
+namespace TestProject.Entity.Concreate
+{
+    public class ProductImage : IEntity
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string AddedBy { get; set; }
+        public string AddedDate { get; set; }
+    }
+}
